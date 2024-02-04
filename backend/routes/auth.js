e8eb803fcb88e20a7df12888c34cd0aeddf48e28
Amrit <amritsinghal2211@gmail.com>
2024-02-05 00:22:01 +0530

    refrcracted

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,24 +3,32 @@ const router = express.Router();
 const {body ,validationResult} = require('express-validator');
 const User = require('../models/User');
 // Create a User using : Post "api/auth/" Doesnot require Auth
-router.post('/',[
+router.post('/createuser',[
     body('name').isLength({min:3}) ,
     body('email').isEmail() ,
     body('password').isLength({min:5}) ,
 
-] , (req ,res)=>{
+] , async (req ,res)=>{
     const errors = validationResult(req);
     console.log(errors)
     if(!errors.isEmpty()){
         return res.send(400).json({errors:errors.array()});
     }
-    User.create({
+    // check whether the email exist already
+    try{
+    let user = await  User.findOne({email:req.body.email})
+    if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exist"})
+    }
+      user = await User.create({
         name: req.body.name ,
         email: req.body.email ,
         password: req.body.password
-    }).then(user=>res.json(user))
-    .catch(err=>{console.log(err)
-    res.json({error:"Pls enter a unique value for email" , message:err.message})});
-    
+    })
+    res.json(user)
+} catch (error){
+    console.log(error.message);
+    res.status(500).send("Some error occured")
+}
 })
 module.exports = router
