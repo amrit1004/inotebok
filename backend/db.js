86b9054c4c55c64442d8fad658b2f94ffba2b8a1
Amrit <amritsinghal2211@gmail.com>
2024-02-04 18:07:27 +0530

    connection to datbase suceed

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const mongoURI = "mongodb://127.0.0.1:27017/"
+
+async function connectToMongo() {
+   await mongoose.connect(mongoURI).
+   then(()=> console.log("Connected to Mongo Successfully")).
+   catch(err => console.log(err));
+ }
+ 
+ module.exports = connectToMongo;
